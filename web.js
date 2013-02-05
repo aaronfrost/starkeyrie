904f0573916c0f2ffc579a4133066279128ba0fd
Gabriel Dayley <gabriel.dayley@gmail.com>
2013-02-05 15:41:12 -0700

    twilio

--- web.js
+++ web.js
@@ -35,7 +35,7 @@ app.post("/sms/hello/:name", function(request, response){
 app.post("/sms/reply/c1", function(request, response){
     var twiml = new twilio.TwimlResponse();
 
-    console.log("got text", request.params);
+    console.log("got text", request.body);
 
 
     twiml.sms('http://twilio.com');
