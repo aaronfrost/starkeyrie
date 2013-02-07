7c7e4ebec051c37ad2325359c116c5b7c94edfbe
Gabriel Dayley <gabriel.dayley@gmail.com>
2013-02-07 16:49:02 -0700

    added final mp3

--- routes/host.js
+++ routes/host.js
@@ -2,8 +2,109 @@ var http = require('http');
 
 module.exports = function(app, server, twilio){
     var io = require('socket.io').listen(server);
-    var client = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN)
+    var client = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+
+    function parseSms(msg){
+        var parts = msg.split(/\W(.+)?/);
+
+        return {
+            id: parts[0] || "UNKNOWN",
+            msg: parts[1] || ""
+        }
+    }
 
 
+    /*
+     // Send an sms message
+    app.post("/twilio/sms/send", function(request, response){
+        var msg = request.body;
+        console.log(msg);
 
+        client.sendSms({
+            to: msg.to,
+            from: msg.from,
+            body: msg.body
+        }, function(err, responseData){
+            console.log('error', err, responseData);
+        });
+
+        response.send('OK');
+    });
+
+
+    // Make a phone call
+    app.post("/twilio/voice/call/:number", function(request, response){
+        var number = request.params.number;
+
+        client.makeCall({
+            to:'+' + number,
+            from:'+18016236842',
+            url: 'http://stark-eyrie-7115.herokuapp.com/twilio/sayhello'
+        });
+
+        response.end("ok");
+    });
+    */
+
+    app.post("/host/voice", function(request, response){
+        var twiml = new twilio.TwimlResponse();
+        twiml.say('Hello there. Are you having fun yet?')
+            .pause({length: 2})
+            .say('Google Developer Group Utah rocks, thanks for coming', {voice: 'woman'});
+
+        response.writeHead(200, {'Content-Type': 'text/xml'});
+        response.end(twiml.toString());
+    });
+
+    app.post("/host/sms", function(request, response){
+        var twiml = new twilio.TwimlResponse();
+
+        console.log(request.body);
+
+        var textBody = request.body.Body;
+
+
+        if(textBody.indexOf('wind') > -1 || textBody.indexOf('blows') > -1){
+            // WINNER
+            io.socket.emit('c3', request.body);
+        }
+        else{
+            var text = parseSms(textBody);
+
+            switch(text.id.toUpperCase()){
+                case 'GDG':
+                    twiml.sms("Hi "+ text.msg);
+                    io.sockets.emit('c1', request.body);
+                    break;
+                case 'CALLME':
+                    twiml.sms("mp3 http://stark-eyrie-7115.herokuapp.com/cmm.mp3");
+                    break;
+                case 'TWILIOROCKS':
+                    io.sockets.emit('c2', request.body);
+                    break;
+                case '33':
+                    client.makeCall({
+                        to:request.body.From,
+                        from:'+18016236842',
+                        url: 'http://stark-eyrie-7115.herokuapp.com/host/congrats'
+                    });
+                    break;
+                default:
+                    twiml.sms("Are you ready to play Survivor, GDG Utah?");
+                    io.sockets.emit('c0', request.body);
+            }
+        }
+
+        response.writeHead(200, {'Content-Type': 'text/xml'});
+        response.end(twiml.toString());
+    });
+
+    app.post('/host/congrats', function(req, res){
+        var twiml = new twilio.TwimlResponse();
+        twiml.pause({length: 3});
+        twiml.say('Any way the wind blows, doesn\'t really matter two me');
+
+        res.writeHead(200, {'Content-Type': 'text/xml'});
+        res.end(twiml.toString());
+    });
 };
--- routes/player.js
+++ routes/player.js
@@ -1,6 +1,141 @@
 var http = require('http');
 
 /* Example web services. Would normally communicate with a database to get the data */
-module.exports = function(app){
+module.exports = function (app, server, twilio) {
+    var client2 = twilio(process.env.TWILIO_SID2, process.env.TWILIO_AUTH_TOKEN2);
+    var client2Mp3;
+    var voice = {voice:'woman', language:'en-gb'};
 
+    app.get("/client2/start", function (req, res) {
+        client2.sendSms({
+            to:'+18016236842',
+            from:'+18017585121',
+            body:'c1 Aaron Frost'
+        }, function (err, responseData) {
+            console.log('error', err, responseData);
+        });
+
+        res.end('OK');
+    });
+
+    app.get("/client2/start2", function (req, res) {
+        client2.sendSms({
+            to:'+18016236842',
+            from:'+18017585121',
+            body:'c2 Aaron Frost'
+        }, function (err, responseData) {
+            console.log('error', err, responseData);
+        });
+
+        res.end('OK');
+
+    });
+
+    app.get("/client2/start3", function (req, res) {
+        client2.makeCall({
+            to:'+18014486681',
+            from:'+18017585121',
+            url:'http://stark-eyrie-7115.herokuapp.com/client2/askfornum'
+        });
+
+        res.end('OK');
+    });
+
+    app.post('/client2/sms', function (req, res) {
+        console.log(req.body.Body);
+        client2Mp3 = req.body.Body.split(' ')[1];
+//        res.end('AARONs ERROR');
+
+        client2.makeCall({
+            to:'+18014486681',
+            from:'+18017585121',
+            url:'http://stark-eyrie-7115.herokuapp.com/client2/sayhello'
+        });
+
+        res.end('OK');
+    });
+
+    app.post('/client2/voice', function (req, res) {
+
+        var twiml = new twilio.TwimlResponse();
+
+        twiml.pause({length:1});
+        twiml.say('Hello, say something');
+        twiml.record({transcribe:true, transcribeCallback:'http://stark-eyrie-7115.herokuapp.com/client2/transcription'});
+
+        res.writeHead(200, {'Content-Type':'text/xml'});
+        res.end(twiml.toString());
+
+    });
+
+    app.post('/client2/transcription', function (req, res) {
+        console.log(req.body.TranscriptionText);
+
+
+        //TODO: SEND TEXT
+
+    });
+
+    app.post('/client2/sayhello', function (req, res) {
+        var twiml = new twilio.TwimlResponse();
+        twiml.say('Hello there. Are you having fun yet?', voice)
+            .pause({length:2})
+            .play(client2Mp3);
+
+        res.writeHead(200, {'Content-Type':'text/xml'});
+        res.end(twiml.toString());
+
+    });
+
+    app.post('/client2/askfornum', function (req, res) {
+        var twiml = new twilio.TwimlResponse();
+        twiml.pause({length:2})
+            .say('Hello, this is round 3', voice)
+            .pause({length:1})
+            .gather({
+                action:'http://stark-eyrie-7115.herokuapp.com/client2/numCallback',
+                numDigits:5
+            }, function () {
+                this.say("Please enter five numbers", voice);
+            });
+
+        res.writeHead(200, {'Content-Type':'text/xml'});
+        res.end(twiml.toString());
+    });
+
+    app.post('/client2/numCallback', function (req, res) {
+        console.log(req.body.Digits);
+        var digits = req.body.Digits.split('');
+
+        var total = 0;
+        digits.forEach(function (d) {
+            total += parseInt(d);
+        });
+
+        var twiml = new twilio.TwimlResponse();
+        twiml.say('Well Mister Wisenheimer, your total is ' + total, {voice:'woman', language:'en-gb'});
+
+        client2.sendSms({
+            to:'+18016236842',
+            from:'+18017585121',
+            body:'c3 ' + digits
+        }, function (err, responseData) {
+            console.log('error', err, responseData);
+        });
+
+        res.writeHead(200, {'Content-Type':'text/xml'});
+        res.end(twiml.toString());
+    });
+
+/*
+    app.get("/client2/start", function (req, res) {
+        client2.makeCall({
+            to:'+18014486681',
+            from:'+18017585121',
+            url:'http://stark-eyrie-7115.herokuapp.com/client2/askfornum'
+        });
+
+        res.end('CLIENT2 OK3');
+    });
+    */
 };
