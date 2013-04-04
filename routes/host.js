8d0ae5ed912a4d1d00c979b5f3b5888dc013ab80
Gabriel Dayley <gabriel.dayley@gmail.com>
2013-04-04 09:03:10 -0600

    refactor

--- routes/host.js
+++ routes/host.js
@@ -71,7 +71,7 @@ module.exports = function(app, server, twilio){
     });
 
     app.post("/host/sms", function(request, response){
-        
+
 
         var twiml = new twilio.TwimlResponse();
         console.log("SMS INCOMING >>>>>>>>>>>",  request.body);
